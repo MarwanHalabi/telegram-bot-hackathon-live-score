3ec19d642eaef0f939dc26f47447ed0fe58c2042
Mahran Abbasi <mahran.abasi@gmail.com>
2020-02-12 23:00:30 +0200

    quick fix

--- Bot.py
+++ Bot.py
@@ -1,10 +1,4 @@
-
-from datetime import datetime
-
 import requests
-
-import Message
-from Models import Matches_model
 from config import *
 
 
@@ -12,21 +6,3 @@ def open_tele():
     requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 
 
-
-def send_game_data():
-    active_matches = Matches_model.get_subscription_list()
-    for match in active_matches:
-        score = match["home_team"] + ": " + str(match["home_team_score"]) + ", " + match["visitor_team"] + ": " + str(match["visitor_team_score"])
-        for user in match["users"]:
-            print("here" + str(user))
-            Message.parseSend(TOKEN, str(user), score)
-
-
-score_counter = 18;
-def alter_data():
-    global score_counter
-    game_result = {"match_id": 1, "last_updated": datetime.now(),
-                   "home_team_score": score_counter,
-                   "visitor_team_score": 2}
-    Matches_model.update_score(game_result)
-    score_counter += 2
