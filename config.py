4918e8f73e8482ef05dd3443ba3104399359df89
מרוואן <m.halvi@hotmail.com>
2020-02-12 09:16:00 +0200

    base empty folders

---
+++ config.py
@@ -0,0 +1,4 @@
+
+TOKEN = '1090954125:AAHA-WX4zsCnVr9pcm2nRXuwuuXDqaM1cQA'
+TELEGRAM_INIT_WEBHOOK_URL = 'https://api.telegram.org/bot{}/setWebhook?url=https://60995f92.ngrok.io/message' \
+    .format(TOKEN)
