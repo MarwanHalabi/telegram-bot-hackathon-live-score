38aa67dbb3fd47ffe4804b3ab3b45c54048956da
Yazan Abbasi <yazanabbasi96@gmail.com>
2020-02-12 11:44:05 +0200

    edit db

--- Models/Matches_model.py
+++ Models/Matches_model.py
@@ -1,11 +1,6 @@
 from Models.sql_helper import insert_to_DB
 
 
-def add_match(match_id,team_one ,start_time):
-    query = "insert into today_matches (match_id,start_time) values ({},{})".format(match_id,start_time)
+def add_match(match_id, team_one, start_time):
+    query = "insert into today_matches (match_id,start_time) values ({},{})".format(match_id, start_time)
     insert_to_DB(query)
-
-
-
-
-
--- database/database.sql
+++ database/database.sql
@@ -1,17 +1,25 @@
-create database league;
 use league;
+
+
+
+create table today_matches(
+    match_id int not null primary key,
+    team_one varchar(50),
+    team_two varchar(50),
+    start_time datetime
+);
 
 create table match_status(
     match_id int not null primary key,
-    team_one_score int,
-    team_two_score int,
+    team_one_score int default 0,
+    team_two_score int  default 0,
     last_updated datetime,
-    start_time datetime
+    FOREIGN KEY(match_id) REFERENCES today_matches(match_id)
 );
 
 create table match_subscription(
     user_id int not null, 
     match_id int not null,
-    FOREIGN KEY(match_id) REFERENCES match_status(match_id),
+    FOREIGN KEY(match_id) REFERENCES today_matches(match_id),
     CONSTRAINT PK_subscription PRIMARY KEY (user_id,match_id)
 );
